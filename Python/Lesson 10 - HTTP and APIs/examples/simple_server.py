11bf067bac29fbb6676a4af222fb0b2da4898d9c
shafe123 <ethan.h.shafer@gmail.com>
2026-06-22 22:38:14 -0400

    expands http lesson

--- Python/Lesson 10 - HTTP and APIs/examples/simple_server.py
+++ Python/Lesson 10 - HTTP and APIs/examples/simple_server.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import request
 import os
 import json
+import base64
+import uuid
 
 app = Flask(__name__)
 
@@ -10,17 +12,35 @@ app = Flask(__name__)
 def hello_world():
     result = "<h1>Hello, world</h1>"
     if request.args:
-        result += "\n- Server received: " + json.dumps({key: val for key, val in request.args.items()})
+        result += "\n- Server received: " + json.dumps(
+            {key: val for key, val in request.args.items()}
+        )
     return result
+
+
+@app.route("/image", methods=["POST"])
+def process_image():
+    try:
+        if request.content_type.lower() != "image/png":
+            raise Exception('Content type must be "image/png"')
+        path = os.path.join("./", str(uuid.uuid4()) + ".png")
+        with open(path, "wb") as file:
+            file.write(base64.b64decode(request.data))
+
+        return f"Image saved to {path}"
+    except Exception as e:
+        return f"Error processing image {e}", 500
 
 
 @app.route("/teapot")
 def error_val():
     return "I'm a teapot", 418
 
+
 @app.route("/requestType", methods=["GET", "POST", "DELETE", "PATCH", "PUT"])
 def request_type():
     return f"Server received {request.method} HTTP request"
+
 
 @app.route("/api")
 def api():
@@ -39,8 +59,10 @@ def api():
 
     return json.dumps(result)
 
+
 def run():
     app.run(host="0.0.0.0", port=5000, debug=True)
+
 
 if __name__ == "__main__":
     run()
